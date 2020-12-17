2f3c602ea04a0d59a5f99d8b2c3d32a90a399fac
kotani akihiro <akihiroetegami@gmail.com>
2020-12-17 11:29:23 +0900

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBoe7G-_V-QHNC7RwJ0IOvFnGfxW_TC4p4",
-    authDomain: "simplebingo.firebaseapp.com",
-    databaseURL: "https://simplebingo.firebaseio.com",
-    projectId: "firebase-simplebingo",
-    storageBucket: "firebase-simplebingo.appspot.com",
-    messagingSenderId: "456886427329",
-    appId: "1:456886427329:web:f9e0eb0157e4f329"
+
   }
 };
 
